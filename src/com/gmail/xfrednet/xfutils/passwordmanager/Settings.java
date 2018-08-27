cb4ae6d54fcac45f615ad7f70d9dc00de6b87188
xFrednet <xFrednet@gmail.com>
2018-08-27 13:49:30 +0200

    Added my LICENSE
    And added the salt file to the git ignore file there is no reason to share the salt. They should be newly generated anyways

--- src/com/gmail/xfrednet/xfutils/passwordmanager/Settings.java
+++ src/com/gmail/xfrednet/xfutils/passwordmanager/Settings.java
@@ -1,3 +1,35 @@
+/******************************************************************************
+ * xFutils-Password-Manager - A simple password manager                        *
+ *                 <https://github.com/xFrednet/xFutils-Password-Manager>      *
+ *                                                                             *
+ * =========================================================================== *
+ * Copyright (C) 2018, xFrednet <xFrednet@gmail.com>                           *
+ *                                                                             *
+ * This software is provided 'as-is', without any express or implied warranty. *
+ * In no event will the authors be held liable for any damages arising from    *
+ * the use of this software.                                                   *
+ *                                                                             *
+ * Permission is hereby granted, free of charge, to anyone to use this         *
+ * software for any purpose, including the rights to use, copy, modify,        *
+ * merge, publish, distribute, sublicense, and/or sell copies of this          *
+ * software, subject to the following conditions:                              *
+ *                                                                             *
+ *   1.  The origin of this software must not be misrepresented; you           *
+ *       must not claim that you wrote the original software. If you           *
+ *       use this software in a product, an acknowledgment in the              *
+ *       product documentation would be greatly appreciated but is not         *
+ *       required                                                              *
+ *                                                                             *
+ *   2.  Altered source versions should be plainly marked as such, and         *
+ *       must not be misrepresented as being the original software.            *
+ *                                                                             *
+ *   3.  This code should not be used for any military or malicious            *
+ *       purposes.                                                             *
+ *                                                                             *
+ *   4.  This notice may not be removed or altered from any source             *
+ *       distribution.                                                         *
+ *                                                                             *
+ ******************************************************************************/
 package com.gmail.xfrednet.xfutils.passwordmanager;
 
 import java.awt.*;
